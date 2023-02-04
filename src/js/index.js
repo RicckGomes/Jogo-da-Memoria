dcc2709bf95f6e9794e1ddf3a81a31d04f332968
Ricck Gomes <richardgomesteixeira@yahoo.com.br>
2023-02-04 19:31:15 -0300

    iniciei o JS

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+var nome = prompt("Olá, qual o seu nome?");
+
+alert(nome + ", seja muito bem vind@!!! Vamos testar sua memória?!");
+
+var h1 = document.getElementById("nada");
+
+h1.innerText = "Boa sorte " + nome;
+
+h1.style.color = "bisque";
+
+var cartoes = document.querySelectorAll(".cartao");
+
+var existeCartaoVirado = false;
+
+var primeiroCartao, segundoCartao;
+
+function virarCartao () {
+    this.classList.add("virado");
+
+    if (existeCartaoVirado == false){
+        existeCartaoVirado = true;
+        primeiroCartao = this;
+    } else {
+        segundoCartao = this;
+        existeCartaoVirado = true;
+        
+        if (primeiroCartao.dataset.info === segundoCartao.dataset.info){
+            primeiroCartao.removeEventListener("click", virarCartao);
+            segundoCartao.removeEventListener("click", virarCartao);
+        } else {
+            setTimeout (function () {
+                primeiroCartao.classList.remove("virado");
+                segundoCartao.classList.remove("virado");
+            }, 2000);
+        }
+    }
+}
+
+cartoes.forEach(function (cartao){
+    cartao.addEventListener("click", virarCartao);
+});
